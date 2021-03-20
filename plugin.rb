425f3b227b9a6fcfbb0e9eadf4a1a9e4b7fce7c6
Jessica Hamilton <jessica.l.hamilton@gmail.com>
2021-03-20 13:05:28 +1300

    An empty plugin file

---
+++ plugin.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# name: form
+# about: A form plugin for Discourse
+# version: 0.1
+# authors: Jessica Hamilton (jessicah)
+# url: https://github.com/jessicah/discourse-form-plugin
+
+PLUGIN_NAME ||= "discourse_form"
+DATA_PREFIX ||= "data-form-"
